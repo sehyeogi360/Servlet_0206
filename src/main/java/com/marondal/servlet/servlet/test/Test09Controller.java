8c4511528974fbe9cfeda79db95f027bd7e7cfcb
DESKTOP-61D76IS\user <forthewy@gmail.com>
2023-02-07 18:10:50 +0900

    [임세혁] -전체수정

--- src/main/java/com/marondal/servlet/servlet/test/Test09Controller.java
+++ src/main/java/com/marondal/servlet/servlet/test/Test09Controller.java
@@ -14,18 +14,19 @@ public class Test09Controller extends HttpServlet{
 	
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
+		// 왜 포스트가 되어야 하느냐? 자기소개서 부분이 길어서 200자 이상이므로 큰길이의 데이터이므로
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
 		
+		// 이름과 자기소개서를 전달 받고, html 화면 구성
 		String name = request.getParameter("name");
 		String introduce = request.getParameter("introduce");
 		
-		out.println("<html><head><title>입사지원</title></head><body>");
+		out.println("<html><head><title>지원결과</title></head><body>");
 		
-		out.println("<h2>" + name + "님 지원이 완료 되었습니다." + "</h2> <br>");
-		
+		out.println("<h1>" + name + "님 지원이 완료 되었습니다." + "</h1> <hr>");
+		// 404는 무조건 웹 브라우저랑 url 확인
 		out.println("<h3>지원 내용</h3>");
 		out.println(introduce);
 		
--- src/main/java/com/marondal/servlet/servlet/test/Test10Controller.java
+++ src/main/java/com/marondal/servlet/servlet/test/Test10Controller.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 @WebServlet("/servlet/test10")
 public class Test10Controller extends HttpServlet {
 
@@ -21,25 +22,20 @@ public class Test10Controller extends HttpServlet {
 		
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
-		
-		
-		
+			
 		
 		out.println("<html><head><title>로그인</title></head><body>");		
-		
-		if(!userMap.get("id").equals(id) && !userMap.get("password").equals(pwd)){
-        	out.println("아이디와 비밀번호를 확인해주세요.");
-        	out.println("</body></html>");
-        } else if(!userMap.get("id").equals(id) && userMap.get("password").equals(pwd)) {
-        	out.println("id가 일치하지 않습니다.");
-        	out.println("</body></html>");	
-        } else if(userMap.get("id").equals(id) && !userMap.get("password").equals(pwd)) {
-        	out.println("password가 일치하지 않습니다.");
-        	out.println("</body></html>");	
+		// 전달 받은 아이디가 맵의 id와 일치하지 않는다면, 경고 문구 
+		if(!userMap.get("id").equals(id) && !userMap.get("password").equals(pwd)){ // 자바스크립트가 아니기에 무조건 equals 요거에대응 하는 키값을 넣어준다.
+        	out.println("<h2>아이디와 비밀번호를 확인해주세요.</h2>"); //이건 굳이 없어도되는듯.        	
+        } else if(!userMap.get("id").equals(id) && userMap.get("password").equals(pwd)) {// 이것도 아이디 조건만
+        	out.println("<h2>id가 일치하지 않습니다.</h2>");       
+        } else if(userMap.get("id").equals(id) && !userMap.get("password").equals(pwd)) {// 이것도 비번 조건만
+        	out.println("<h2>password가 일치하지 않습니다.</h2>");       		
         } else {
-        	out.println("<h1>" + userMap.get("name")+ "님 환영합니다." + "</h1>");		
-			out.println("</body></html>");	
+        	out.println("<h1>" + userMap.get("name")+ "님 환영합니다." + "</h1>");						
 		}
+		out.println("</body></html>");
 		
 	}
 	
